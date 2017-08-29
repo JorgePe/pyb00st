d5833946d1f658becb205ab59ffb81eb814de6cf
Jorge Pereira <JorgePe@users.noreply.github.com>
2017-08-29 01:11:27 +0100

    Update test01.py

--- examples/test01.py
+++ examples/test01.py
@@ -3,10 +3,9 @@
 from pyb00st import movehub
 
 mymovehub = movehub("00:16:53:A4:CD:7E", "hci0")
-if mymovehub.is_connected() :
-    print("Connection OK")
-    print( mymovehub.getAddress() )
-    print( mymovehub.getName() )
-
-else:
-    print("Connection Failed")
+if mymovehub.is_connected() == False :
+    print("No connection")
+    mymovehub.connect()
+    
+print( mymovehub.getAddress() )
+print( mymovehub.getName() )
