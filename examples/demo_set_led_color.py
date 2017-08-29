e2f3a3ed5a7a09bd81d72401ca20510908da5c11
Jorge Pereira <JorgePe@users.noreply.github.com>
2017-08-29 09:04:33 +0100

    Add files via upload

---
+++ examples/demo_set_led_color.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from pyb00st import MoveHub
+from pyb00st.constants import *
+from time import sleep
+
+mymovehub = MoveHub("00:16:53:A4:CD:7E", "hci0")
+
+for color in LED_COLORS :
+    mymovehub.set_led_color(color)
+    sleep(1)
