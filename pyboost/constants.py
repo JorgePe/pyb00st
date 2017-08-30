d5995b4aa5da3ddde3ac1c00552ef0b3f15c8b9f
Jorge Pereira <JorgePe@users.noreply.github.com>
2017-08-30 16:36:13 +0100

    Update constants.py

--- pyboost/constants.py
+++ pyboost/constants.py
@@ -4,7 +4,6 @@
 
 HANDLE = 0x0E
 
-# Commands:
 
 # Commands for setting RGB LED color
 SET_LED_OFF = b'\x08\x00\x81\x32\x11\x51\x00\x00'
@@ -34,3 +33,29 @@ SET_LED_COLOR = [SET_LED_OFF,
 # Colors:
 LED_COLORS = ['OFF','PINK','PURPLE','BLUE','LIGHTBLUE','CYAN','GREEN','YELLOW','ORANGE','RED','WHITE']
 
+# Motors:
+
+MOTOR_A = bytes([0x37])
+MOTOR_B = bytes([0x38])
+MOTOR_AB = bytes([0x39])
+MOTOR_C = bytes([0x01])
+MOTOR_D = bytes([0x02])
+
+# a group of all single motors
+MOTORS = [MOTOR_A, MOTOR_B, MOTOR_AB, MOTOR_C, MOTOR_D]
+
+# a group of 1 is silly but there might be other pairs in the future
+MOTOR_PAIRS = [MOTOR_AB]
+
+# Commands for motors (Timed):
+
+# Motor A, B, C, D: 12-byte commands
+# Motor AB: 13-byte commands
+
+MOTOR_TIMED_INI = b'\x0c\x01\x81'
+MOTOR_TIMED_MID = b'\x11\x09'
+MOTOR_TIMED_END = b'\x64\x7f\x03'
+
+MOTORS_TIMED_INI = b'\x0d\x01\x81'
+MOTORS_TIMED_MID = b'\x11\x0A'
+MOTORS_TIMED_END = b'\x64\x7f\x03'
