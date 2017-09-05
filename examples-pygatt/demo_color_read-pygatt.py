4dd7f0ca83c0da6c4888fb470b7e6fa75da41b97
Jorge Pereira <JorgePe@users.noreply.github.com>
2017-09-05 16:36:07 +0100

    Create demo_color_read-pygatt.py

---
+++ examples-pygatt/demo_color_read-pygatt.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+from pyb00st_pygatt import MoveHub
+from pyb00st_pygatt.constants import *
+
+from time import sleep
+
+MY_MOVEHUB_ADD = '00:16:53:A4:CD:7E'
+
+# not used - need to find how where on pygatt we can choose the hci controller
+MY_BTCTRLR_HCI = 'hci0'
+
+mymovehub = MoveHub(MY_MOVEHUB_ADD, MY_BTCTRLR_HCI)
+mymovehub.subscribe(mymovehub.read_color_sensor)
+mymovehub.init_color_sensor(PORT_C)
+
+while True:
+    sleep(0.2)
+    print(mymovehub.last_color)
