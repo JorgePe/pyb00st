b75180dcfd8f1e75025b9320df9367b3bdab7a80
JorgePe <cyberx@pobox.com>
2017-09-08 17:35:38 +0100

    Color Speak example with pyhton-espeak

---
+++ examples-pygatt/demo_color_speak-pygatt.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+from pyb00st_pygatt.movehub import MoveHub
+from pyb00st_pygatt.constants import *
+
+from espeak import espeak # apt install python3-espeak
+from time import sleep
+
+MY_MOVEHUB_ADD = '00:16:53:A4:CD:7E'
+
+# not used - need to find how where on pygatt we can choose the hci controller
+MY_BTCTRLR_HCI = 'hci0'
+
+espeak.set_voice='en'
+
+mymovehub = MoveHub(MY_MOVEHUB_ADD, MY_BTCTRLR_HCI)
+mymovehub.subscribe_color()
+mymovehub.listen_color_sensor(PORT_C)
+
+while True:
+    sleep(1)
+    print(mymovehub.last_color)
+    espeak.synth(mymovehub.last_color)
+
