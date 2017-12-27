11f8dd26ab92886ae5683c9062205458596941db
Frank Schreiner <schreiner@suse.de>
2017-12-28 00:44:48 +0100

    examples/demo_tilt_read.py using B00stConfig

--- examples/demo_tilt_read.py
+++ examples/demo_tilt_read.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python3
 
+from pyb00st import B00stConfig
 from pyb00st.movehub import MoveHub
-from pyb00st.constants import *
+from pyb00st.constants import MODE_HUBTILT_BASIC, TILT_BASIC_VALUES, TILT_BASIC_TEXT
 
 from time import sleep
 
-MY_MOVEHUB_ADD = '00:16:53:A4:CD:7E'
-MY_BTCTRLR_HCI = 'hci0'
+CFG = B00stConfig()
 
-#mymovehub = MoveHub(MY_MOVEHUB_ADD, 'BlueZ', MY_BTCTRLR_HCI)
-#mymovehub = MoveHub(MY_MOVEHUB_ADD, 'BlueGiga', '')
-mymovehub = MoveHub(MY_MOVEHUB_ADD, 'Auto', MY_BTCTRLR_HCI)
+mymovehub = MoveHub(CFG.MY_MOVEHUB_ADD, 'Auto', CFG.MY_BTCTRLR_HCI)
 
 try:
     mymovehub.start()
@@ -22,6 +20,7 @@ try:
         sleep(0.2)
         if mymovehub.last_hubtilt in TILT_BASIC_VALUES:
             print(TILT_BASIC_TEXT[mymovehub.last_hubtilt])
+except KeyboardInterrupt:
+    print("\nCTRL-C pressed! Exiting ...")
 finally:
     mymovehub.stop()
-
