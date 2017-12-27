f9a797bb08c014fbb5a0dc5a98f03b2b78515a7a
Frank Schreiner <schreiner@suse.de>
2017-12-27 23:30:15 +0100

    examples/demo_colordist.py using B00stConfig

--- examples/demo_colordist.py
+++ examples/demo_colordist.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 
+from pyb00st import B00stConfig
 from pyb00st.movehub import MoveHub
-from pyb00st.constants import *
+from pyb00st.constants import PORT_C
 
 from time import sleep
 
-MY_MOVEHUB_ADD = '00:16:53:A4:CD:7E'
-MY_BTCTRLR_HCI = 'hci0'
+CFG = B00stConfig()
 
-mymovehub = MoveHub(MY_MOVEHUB_ADD, 'BlueZ', MY_BTCTRLR_HCI)
+mymovehub = MoveHub(CFG.MY_MOVEHUB_ADD, 'BlueZ', CFG.MY_BTCTRLR_HCI)
 
 try:
     mymovehub.start()
@@ -19,5 +19,7 @@ try:
         sleep(0.2)
         print('Color: {} Distance: {}'.format(mymovehub.last_color_C, mymovehub.last_distance_C))
 
+except KeyboardInterrupt:
+    print("\nCTRL-C pressed! Exiting ...")
 finally:
     mymovehub.stop()
