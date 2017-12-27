cca06efd1a5da3afa682d54e6b8cd8e53237efbf
Frank Schreiner <schreiner@suse.de>
2017-12-27 17:00:30 +0100

    make setup.py pylint/flake8 ready

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
+'''setup.py - build and install libraries'''
+
 from distutils.core import setup
 
 setup(
     name='pyb00st',
     version='0.1.0',
-    packages=['pyb00st',],
+    packages=['pyb00st'],
     license='MIT License',
     long_description=open('README.md').read(),
     url="git@github.com:M0ses/pyb00st",
