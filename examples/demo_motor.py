1c8c71a6fad300b677441b6f6e3888ea382fec6a
Frank Schreiner <schreiner@suse.de>
2017-12-27 16:37:26 +0100

    make examples/demo_motor.py pylint/flake8 ready

--- examples/demo_motor.py
+++ examples/demo_motor.py
@@ -1,54 +1,64 @@
 #!/usr/bin/env python3
-
-from pyb00st import B00stConfig
-from pyb00st.movehub import MoveHub
-from pyb00st.constants import *
+'''Example script to show how to control motors'''
 
 from time import sleep
 
-CFG = B00stConfig()
+from pyb00st import B00stConfig
+from pyb00st.movehub import MoveHub
+from pyb00st.constants import MOTOR_A, MOTOR_AB
 
-mymovehub = MoveHub(CFG.MY_MOVEHUB_ADD, 'BlueZ', CFG.MY_BTCTRLR_HCI)
 
-try:
-    mymovehub.start()
+def run():
+    '''
+    This function is required for pylint, without cfg and mymovehub are
+    treated as constants
+    '''
+    cfg = B00stConfig()
+    mymovehub = MoveHub(cfg.MY_MOVEHUB_ADD, 'BlueZ', cfg.MY_BTCTRLR_HCI)
 
-    # turn motor A ON for 1000 ms at 100% duty cycle in both directions
-    mymovehub.run_motor_for_time(MOTOR_A, 1000, 100)
-    sleep(1)
-    mymovehub.run_motor_for_time(MOTOR_A, 1000, -100)
-    sleep(1)
+    try:
+        mymovehub.start()
 
-    sleep(0.5)
+        # turn motor A ON for 1000 ms at 100% duty cycle in both directions
+        mymovehub.run_motor_for_time(MOTOR_A, 1000, 100)
+        sleep(1)
+        mymovehub.run_motor_for_time(MOTOR_A, 1000, -100)
+        sleep(1)
 
-    # rotate motor 90 degrees at 100% duty cycle in both directions
-    mymovehub.run_motor_for_angle(MOTOR_A, 90, 100)
-    sleep(0.5)
-    mymovehub.run_motor_for_angle(MOTOR_A, 90, -100)
+        sleep(0.5)
 
-    sleep(0.5)
+        # rotate motor 90 degrees at 100% duty cycle in both directions
+        mymovehub.run_motor_for_angle(MOTOR_A, 90, 100)
+        sleep(0.5)
+        mymovehub.run_motor_for_angle(MOTOR_A, 90, -100)
 
-    # turn pair AB ON for 1000 ms at 100% duty cycle in both direction
-    mymovehub.run_motors_for_time(MOTOR_AB, 1000, 100, 100)
-    sleep(1)
-    mymovehub.run_motors_for_time(MOTOR_AB, 1000, 100, -100)
-    sleep(1)
-    mymovehub.run_motors_for_time(MOTOR_AB, 1000, -100, -100)
-    sleep(1)
-    mymovehub.run_motors_for_time(MOTOR_AB, 1000, -100, 100)
-    sleep(1)
+        sleep(0.5)
 
-    sleep(0.5)
+        # turn pair AB ON for 1000 ms at 100% duty cycle in both direction
+        mymovehub.run_motors_for_time(MOTOR_AB, 1000, 100, 100)
+        sleep(1)
+        mymovehub.run_motors_for_time(MOTOR_AB, 1000, 100, -100)
+        sleep(1)
+        mymovehub.run_motors_for_time(MOTOR_AB, 1000, -100, -100)
+        sleep(1)
+        mymovehub.run_motors_for_time(MOTOR_AB, 1000, -100, 100)
+        sleep(1)
 
-    # rotate pair AB 90 degrees at 100% duty cycle in both direction
-    mymovehub.run_motors_for_angle(MOTOR_AB, 90, 100, 100)
-    sleep(0.5)
-    mymovehub.run_motors_for_angle(MOTOR_AB, 90, 100, -100)
-    sleep(0.5)
-    mymovehub.run_motors_for_angle(MOTOR_AB, 90, -100, -100)
-    sleep(0.5)
-    mymovehub.run_motors_for_angle(MOTOR_AB, 90, -100, 100)
-    sleep(0.5)
+        sleep(0.5)
 
-finally:
-    mymovehub.stop()
+        # rotate pair AB 90 degrees at 100% duty cycle in both direction
+        mymovehub.run_motors_for_angle(MOTOR_AB, 90, 100, 100)
+        sleep(0.5)
+        mymovehub.run_motors_for_angle(MOTOR_AB, 90, 100, -100)
+        sleep(0.5)
+        mymovehub.run_motors_for_angle(MOTOR_AB, 90, -100, -100)
+        sleep(0.5)
+        mymovehub.run_motors_for_angle(MOTOR_AB, 90, -100, 100)
+        sleep(0.5)
+
+    finally:
+        mymovehub.stop()
+
+
+if __name__ == '__main__':
+    run()
